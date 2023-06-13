2ec225e77cdf09ee6640e93243514d7f8f27bccb
pearsncheers <raulrahulroy@gmail.com>
2023-06-13 18:29:48 +0530

    Update qr.py

--- qr.py
+++ qr.py
@@ -18,5 +18,22 @@ qr.make(fit=True)
 # Get the QR code image
 qr_image = qr.make_image(fill_color="black", back_color="white")
 
+
+CREATE EXTERNAL TABLE IF NOT EXISTS svoc_lookup (
+  svoc_id DECIMAL(38, 10),
+  mobile STRING
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+  'separatorChar' = ',',
+  'quoteChar' = '"',
+  'escapeChar' = '\\'
+)
+LOCATION 's3://your-bucket-name/path/to/csv-data/'
+TBLPROPERTIES (
+  'skip.header.line.count' = '1'
+);
+
+
 # Save the QR code image
 qr_image.save("qr_code.png")
